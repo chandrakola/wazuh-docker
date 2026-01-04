f0959aa511c92aadec74c24eff30946accf92263
Chandra Kola <5986878+chandrakola@users.noreply.github.com>
2026-01-03 19:45:29 -0500

    feat: configure wazuh single-node manager and add agent setup

---
+++ wazuh-agent/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Register agent if key is missing
+if [ ! -s /var/ossec/etc/client.keys ]; then
+    # Use ENV vars with defaults if not set
+    MANAGER_IP=${WAZUH_REGISTRATION_SERVER:-host.docker.internal}
+    MANAGER_PORT=${WAZUH_REGISTRATION_PORT:-1515}
+    
+    /var/ossec/bin/agent-auth -m "$MANAGER_IP" -p "$MANAGER_PORT" -d
+fi
+
+# Start agent in foreground
+echo "Starting Wazuh Agent..."
+/var/ossec/bin/wazuh-agentd
+/var/ossec/bin/wazuh-logcollector
+/var/ossec/bin/wazuh-syscheckd
+/var/ossec/bin/wazuh-modulesd
+
+# Tail logs to keep container alive
+tail -f /var/ossec/logs/ossec.log
